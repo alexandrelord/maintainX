9684374c028f92dede0c5c1a6e31ad92f7b9b89e
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2023-01-19 14:36:08 -0500

    add workorders controller functions and services

---
+++ backend/src/controllers/work-orders/workOrders.ts
@@ -0,0 +1,46 @@
+import { NextFunction, Request, Response } from 'express';
+import { queryWorkOrders, queryWorkOrder, insertWorkOrder, updateWorkOrderStatus } from './workOrders.services';
+
+export const getWorkOrders = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const workOrders = await queryWorkOrders();
+		res.status(200).json({ data: workOrders });
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const showWorkOrder = async (req: Request, res: Response, next: NextFunction) => {
+	const { id } = req.params;
+
+	try {
+		const workOrder = await queryWorkOrder(Number(id));
+		res.status(200).json({ data: workOrder });
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const createWorkOrder = async (req: Request, res: Response, next: NextFunction) => {
+	const { workOrderName } = req.body;
+	const { assigneesId } = req.body;
+
+	try {
+		await insertWorkOrder(workOrderName, assigneesId);
+
+		res.status(201).json({ message: 'Work order created successfully.' });
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const updateWorkOrder = async (req: Request, res: Response, next: NextFunction) => {
+	const { id } = req.params;
+	const { status } = req.body;
+	try {
+		const updated = updateWorkOrderStatus(Number(id), status);
+		return res.status(200).json({ data: updated });
+	} catch (error) {
+		next(error);
+	}
+};
