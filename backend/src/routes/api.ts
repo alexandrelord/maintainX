647635f219799fce90881f686ba03105721cffb4
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2023-01-19 14:43:50 -0500

    add user routes

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,8 +1,10 @@
 import Router from 'express';
 import workOrderRoutes from './workOrders';
+import userRoutes from './users';
 
 const router = Router();
 
 router.use('/workorders', workOrderRoutes);
+router.use('/users', userRoutes);
 
 export default router;
