eb28b4a1911b9ff957c40391b81eb1d5a48b9212
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2023-01-20 16:25:28 -0500

    setup cors configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,9 +3,11 @@ import chalk from 'chalk';
 import logger from './middleware/logger';
 import apiRoutes from './routes/api';
 import errorHandler from './middleware/errorHandler';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(logger);
 app.use(express.json());
 app.use('/api', apiRoutes);
