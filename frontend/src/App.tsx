be20115d207b6414adfb5c453ea142c9e8dd9143
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2023-01-20 12:45:10 -0500

    install and setup react-router-dom for client-side routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 
 function App() {
     return (
-        <div>
-            <h1>React Typescript</h1>
-        </div>
+        <Routes>
+            <Route element={<Layout />}>
+                <Route path="/" element={<h1>Home</h1>} />
+                <Route path="/workorders" element={<h1>Work Orders</h1>} />
+                <Route path="/new" element={<h1>New Work Order</h1>} />
+                <Route path="/productivity" element={<h1>Productivity</h1>} />
+                <Route path="*" element={<h1>Not Found</h1>} />
+            </Route>
+        </Routes>
     );
 }
 
