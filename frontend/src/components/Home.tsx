0da9ab707925569ca953103d058afb1384fb8f47
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2023-01-24 16:01:45 -0500

    add Home component and refactor styles

---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div className="container">
+            <h1>Welcome to MaintainX</h1>
+            <h3>Your Work Order App</h3>
+            <p>Click on the links above to get started.</p>
+        </div>
+    );
+};
+
+export default Home;
